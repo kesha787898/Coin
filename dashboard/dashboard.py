e85036d7d73467d64c851009eb558296cb019898
kesha787898@gmail.com <kesha787898@gmail.com>
2022-08-16 23:37:36 +0600

    [*] add trying to fix

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,6 +7,7 @@ import dash_core_components as dcc
 from dash.dependencies import Input, Output
 from sqlalchemy import create_engine
 import pandas as pd
+
 engine = create_engine("postgresql://rxkumqwl:4SWbeuIQTKlzQx2P3Tn8TGbulaaX3xGh@abul.db.elephantsql.com/rxkumqwl",
                        echo=False, future=True)
 df = pd.read_sql('prices', engine.connect())
@@ -14,8 +15,9 @@ df = df.groupby(pd.Grouper(key='created_at', freq='60s')).agg({'price': ['mean',
 df.columns = [' '.join(col).strip() for col in df.columns.values]
 df = df.reset_index(level=0)
 app = dash.Dash(__name__)
+server=app.server
 app.layout = html.Div(id='parent', children=[
-    html.H1(id='H1', children='Styling using html components', style={'textAlign': 'center', \
+    html.H1(id='H1', children='Styling using html components', style={'textAlign': 'center',
                                                                       'marginTop': 40, 'marginBottom': 40}),
 
     dcc.Dropdown(id='dropdown',
@@ -32,7 +34,7 @@ app.layout = html.Div(id='parent', children=[
 @app.callback(Output(component_id='bar_plot', component_property='figure'),
               [Input(component_id='dropdown', component_property='value')])
 def graph_update(dropdown_value):
-    fig = go.Figure([go.Scatter(x=df['created_at'], y=df[f"price {dropdown_value}"], \
+    fig = go.Figure([go.Scatter(x=df['created_at'], y=df[f"price {dropdown_value}"],
                                 line=dict(color='firebrick', width=4))
                      ])
 
@@ -41,6 +43,7 @@ def graph_update(dropdown_value):
                       yaxis_title='Prices'
                       )
     return fig
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8050))
